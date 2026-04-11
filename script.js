5a89b17716377673401ed3810a0ae1c8cbc5c2ae
shreyansksingh49 <shreyanshksingh49@gmail.com>
2026-04-11 11:26:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// HOME → RESULTS
+function goToResults() {
+    let hometown = document.getElementById("hometown").value;
+
+    // Save data in browser
+    localStorage.setItem("hometown", hometown);
+
+    // Redirect
+    window.location.href = "results.html";
+}
+
+// RESULTS PAGE LOAD
+window.onload = function () {
+    let hometown = localStorage.getItem("hometown");
+
+    if (document.getElementById("match")) {
+        if (hometown && hometown.toLowerCase() === "kanpur") {
+            document.getElementById("match").innerText =
+                "✅ People from your area live here (High Match)";
+        } else {
+            document.getElementById("match").innerText =
+                "⚠️ Medium Match";
+        }
+    }
+
+    // DETAILS PAGE DATA
+    let selectedPG = localStorage.getItem("pg");
+
+    if (selectedPG === "sunrise") {
+        document.getElementById("pgName").innerText = "Sunrise PG";
+        document.getElementById("pgInfo").innerText =
+            "Rent: ₹7500 | WiFi: Yes | Food: Available";
+        document.getElementById("community").innerText =
+            "Residents from UP & Kanpur";
+    }
+
+    if (selectedPG === "green") {
+        document.getElementById("pgName").innerText = "Green Nest PG";
+        document.getElementById("pgInfo").innerText =
+            "Rent: ₹6500 | WiFi: Yes | Food: No";
+        document.getElementById("community").innerText =
+            "Residents from Bihar";
+    }
+};
+
+// RESULTS → DETAILS
+function viewDetails(pgName) {
+    localStorage.setItem("pg", pgName);
+    window.location.href = "details.html";
+}
+function goBack() {
+    window.location.href = "results.html";
+}
+function goBack() {
+    window.location.href = "results.html";
+}
