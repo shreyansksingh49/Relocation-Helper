f6079f3ec8770a4ee35bc85cb729c9a717a4c7eb
shreyansksingh49 <shreyanshksingh49@gmail.com>
2026-04-14 23:39:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -38,3 +38,5 @@ app.listen(3000, () => console.log("Server running on port 3000"));
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server running");
 });
+const cors = require("cors");
+app.use(cors());
