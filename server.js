03c7d1ddb1f0f032feff094205202ff48289eb96
shreyansksingh49 <shreyanshksingh49@gmail.com>
2026-04-15 21:56:10 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,42 +1,21 @@
-const express = require("express");
 const fs = require("fs");
-const app = express();
+const path = require("path");
 
-app.use(express.json());
-app.use(express.static("public"));
-
-// GET all PGs
 app.get("/pgs", (req, res) => {
-    let data = JSON.parse(fs.readFileSync("./data/pgs.json"));
-    res.json(data);
-});
+    try {
+        const filePath = path.join(__dirname, "data", "pgs.json");
 
-// ADD PG (owner)
-app.post("/add-pg", (req, res) => {
-    let pgs = JSON.parse(fs.readFileSync("./data/pgs.json"));
-    pgs.push(req.body);
-    fs.writeFileSync("./data/pgs.json", JSON.stringify(pgs, null, 2));
-    res.send("PG Added");
-});
+        if (!fs.existsSync(filePath)) {
+            return res.status(500).json({ error: "pgs.json file not found" });
+        }
 
-// LOGIN
-app.post("/login", (req, res) => {
-    let users = JSON.parse(fs.readFileSync("./data/users.json"));
+        const data = fs.readFileSync(filePath, "utf-8");
 
-    let user = users.find(
-        u => u.username === req.body.username && u.password === req.body.password
-    );
-
-    if (user) {
-        res.json({ success: true });
-    } else {
-        res.json({ success: false });
+        res.json(JSON.parse(data));
+    } catch (error) {
+        res.status(500).json({
+            error: "Server error",
+            message: error.message
+        });
     }
 });
-
-app.listen(3000, () => console.log("Server running on port 3000"));
-app.listen(process.env.PORT || 3000, () => {
-    console.log("Server running");
-});
-const cors = require("cors");
-app.use(cors());
